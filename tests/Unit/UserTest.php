33dbcb78cf96b6fc7281d4360c30803b5fbd2789
kashish <ka399@njit.edu>
2018-03-17 19:13:47 -0400

    Test Case 1 Completed:Test Datatype for User Name and ID.

---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\User;
+
+class UserTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testFirstUserNameDataType()
+    {
+        $user = User::find(1);
+        $this -> assertInternalType('string',$user->name);
+    }
+
+    public function testRandomUserIDDataType()
+    {
+        $user = User::inrandomorder()->first();
+        $this -> assertInternalType('int',$user->id);
+    }
+}
